085cc51c4cffcc1458a7e0622f8ada53837f9381
Tiago Pereira <tanaka1790@gmail.com>
2025-02-15 20:51:37 -0300

    first commit

---
+++ missao-pratica-5.0/pratica-05/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+const EventHubReader = require('./scripts/event-hub-reader.js');
+
+const iotHubConnectionString = 'HostName=Fullstack1.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=utBi7eB+ORf7KLZHBsnw1UKHZgiWpfRJNAIoTPETYdw=';
+if (!iotHubConnectionString) {
+  console.error(`A variável de ambiente IotHubConnectionString deve ser especificada.`);
+  return;
+}
+console.log(`Use IoT Hub conexão string [${iotHubConnectionString}]`);
+
+const eventHubConsumerGroup = 'Fullstack_dispositivo';
+console.log(eventHubConsumerGroup);
+if (!eventHubConsumerGroup) {
+  console.error(`Environment variable EventHubConsumerGroup must be specified.`);
+  return;
+}
+console.log(`Using event hub consumer group [${eventHubConsumerGroup}]`);
+
+// Redirect requests to the public subdirectory to the root
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res /* , next */) => {
+  res.redirect('/');
+});
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        console.log(`Broadcasting data ${data}`);
+        client.send(data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+};
+
+server.listen(process.env.PORT || '3000', () => {
+  const port = server.address().port;
+  console.log(`Server is running. Access it at: http://localhost:${port}`);
+});
+
+const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsumerGroup);
+
+(async () => {
+  await eventHubReader.startReadMessage((message, date, deviceId) => {
+    try {
+      const payload = {
+        IotData: message,
+        MessageDate: date || Date.now().toISOString(),
+        DeviceId: deviceId,
+      };
+
+      wss.broadcast(JSON.stringify(payload));
+    } catch (err) {
+      console.error('Error broadcasting: [%s] from [%s].', err, message);
+    }
+  });
+})().catch();
